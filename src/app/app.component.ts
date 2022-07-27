1d54eb9f79d381e92b547a65654d8d263f4b7ec7
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2022-07-27 18:52:24 -0500

    mysterious addition (import type os) fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,8 +42,11 @@ export class AppComponent {
 		searchTerm = searchTerm.toLowerCase();
 		for(let i=0; i < this.sourceList.length; i++) {
 			let name = this.sourceList[i].name.toLowerCase();
+			let type = this.sourceList[i].type.toLowerCase();
 			if (name.indexOf(searchTerm) >= 0) {
 				matchingSatellites.push(this.sourceList[i]);
+			} else if (type.indexOf(searchTerm) >= 0) {
+				matchingSatellites.push(this.sourceList[i])
 			}
 		}
 		// assign this.displayList to be the array of matching satellites
